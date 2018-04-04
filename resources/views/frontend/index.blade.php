511bad007ed6383bd8fdcd57345d95473f4b714f
ima0123 <atsushi@ima0123.com>
2018-04-04 22:35:22 +0900

    Fix geo information

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -305,97 +305,67 @@
 		     Adding Google Map.
 		    ***/
 
-		    /* Calling goMap() function, initializing map and adding markers. */
-		    jQuery('#road_map.directory-map').goMap({
-		        maptype: 'ROADMAP',
-		        latitude: 51.450711,
-		        longitude: 0.2760004,
-		        zoom: 12,
-		        scaleControl: true,
-		        scrollwheel: false,
-					//        group: 'category',
-			        markers: [
-					//            Markers for Doctor Search
-			            {latitude: 51.511622, longitude: -0.150375, icon: ''+url+'map/5.png', html: {
-			                    content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-			                }},
-			            {latitude: 51.524440, longitude: -0.241699, icon: ''+url+'map/3.png', html: {
-			                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-			                }},
-			            {latitude: 51.537388, longitude: -0.077033, icon: ''+url+'map/6.png', html: {
-			                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-			                }},    
-			            {latitude: 51.508930, longitude: -0.347543, icon: ''+url+'map/2.png', html: {
-			                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-			                }},
-			            {latitude: 51.508550, longitude: -0.008712, icon: ''+url+'map/8.png', html: {
-			                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-			            }},
-			            {latitude: 51.549831, longitude: -0.304971, icon: ''+url+'map/1.png', html: {
-			                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-			            }},
-			            {latitude: 51.486562, longitude: -0.310364, icon: ''+url+'map/4.png', html: {
-			                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-			            }},
-			            {latitude: 51.482473, longitude: -0.094542, icon: ''+url+'map/7.png', html: {
-			                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-			            }},
-			        ]
-		    	});
+		    /* Calling goMap() function, initializing map jQuery('#road_map.directory-map').goMap({
+		    //     maptype: 'ROADMAP',
+		    //     latitude: 51.450711,
+		    //     longitude: 0.2760004,
+		    //     zoom: 12,
+		    //     scaleControl: true,
+		    //     scrollwheel: false,
+				// 	//        group: 'category',
+			 //        markers: [
+				// 	//            Markers for Doctor Search
+			 //            {latitude: 51.511622, longitude: -0.150375, icon: ''+url+'map/5.png', html: {
+			 //                    content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
+			 //                }},
+			 //            {latitude: 51.524440, longitude: -0.241699, icon: ''+url+'map/3.png', html: {
+			 //                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
+			 //                }},
+			 //            {latitude: 51.537388, longitude: -0.077033, icon: ''+url+'map/6.png', html: {
+			 //                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
+			 //                }},
+			 //            {latitude: 51.508930, longitude: -0.347543, icon: ''+url+'map/2.png', html: {
+			 //                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
+			 //                }},
+			 //            {latitude: 51.508550, longitude: -0.008712, icon: ''+url+'map/8.png', html: {
+			 //                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
+			 //            }},
+			 //            {latitude: 51.549831, longitude: -0.304971, icon: ''+url+'map/1.png', html: {
+			 //                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
+			 //            }},
+			 //            {latitude: 51.486562, longitude: -0.310364, icon: ''+url+'map/4.png', html: {
+			 //                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
+			 //            }},
+			 //            {latitude: 51.482473, longitude: -0.094542, icon: ''+url+'map/7.png', html: {
+			 //                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
+			 //            }},
+			 //        ]
+		    // 	});and adding markers. */
+		    //
 	    
 
 		    /* Calling goMap() function, initializing map and adding markers. */
 		    jQuery('#road_map').goMap({
 		        maptype: 'ROADMAP',
-		        latitude: 51.450711,
-		        longitude: 0.2760004,
-		        zoom: 13,
+                latitude: 35.5625,
+                longitude: 139.7161,
+		        zoom: 15,
 		        scaleControl: true,
 		        scrollwheel: false,
 				//        group: 'category',
 		        markers: [
-				//            Markers for Doctor Search
-		            {latitude: 51.5131094, longitude: -0.176425, icon: ''+url+'/mapicon/11.png', html: {
-		                    content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-		            {latitude: 51.511218, longitude: -0.147124, icon: ''+url+'/mapicon/12.png', html: {
-		                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-		            {latitude: 51.515918, longitude: -0.219050, icon: ''+url+'/mapicon/13.png', html: {
-		                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-		            {latitude: 51.4941563, longitude: -0.1710176, icon: ''+url+'/mapicon/14.png', html: {
-		                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-		            {latitude: 51.5238585, longitude: -0.0950225, icon: ''+url+'/mapicon/15.png', html: {
-		                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-		            {latitude: 51.4965787, longitude: -0.1169972, icon: ''+url+'/mapicon/16.png', html: {
-		                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-					{latitude: 51.5096738, longitude: -0.2753873, icon: ''+url+'/mapicon/17.png', html: {
-		                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-		            {latitude: 51.4965787, longitude: -0.199223, icon: ''+url+'/mapicon/18.png', html: {
-		                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-		            {latitude: 51.4925041, longitude: -0.2363018, icon: ''+url+'/mapicon/icon.png', html: {
-		                     content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		                }},
-		            {latitude: 51.5202758, longitude: -0.118047, icon: ''+url+'/mapicon/19.png', html: {
-		                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		            }},
-					{latitude: 51.5249492, longitude: -0.2450565, icon: ''+url+'/mapicon/16.png', html: {
-		                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		            }},
-					{latitude: 51.532054, longitude: -0.1639875, icon: ''+url+'/mapicon/15.png', html: {
-		                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		            }},
-					{latitude: 51.5082309, longitude: -0.076872, icon: ''+url+'/mapicon/11.png', html: {
-		                 content: '<h5>ThemeRegion Ads Portal.</h5>Lorem ipsum dolor sit amet,<br/> consectetur adipisicing elit sed <br /><a href="#">Visit Store</a>'
-		            }},
-		            
-		        ]
+                    {latitude: 35.5625, longitude: 139.7161, icon: '' + url + '/mapicon/11.png'},
+                    {latitude: 35.55519, longitude: 139.71788, icon: '' + url + '/mapicon/12.png'},
+                    {latitude: 35.55875, longitude: 139.72415, icon: '' + url + '/mapicon/13.png'},
+                    {latitude: 35.5642, longitude: 138.7161, icon: '' + url + '/mapicon/14.png'},
+                    {latitude: 35.56595, longitude: 139.70959, icon: '' + url + '/mapicon/15.png'},
+                    {latitude: 35.56874, longitude: 139.70102, icon: '' + url + '/mapicon/16.png'},
+                    {latitude: 35.57477, longitude: 139.69856, icon: '' + url + '/mapicon/17.png'},
+                    {latitude: 35.56913, longitude: 139.68733, icon: '' + url + '/mapicon/18.png'},
+                    {latitude: 35.58555, longitude: 139.70637, icon: '' + url + '/mapicon/icon.png'},
+                    {latitude: 35.55215, longitude: 139.74082, icon: '' + url + '/mapicon/icon.png'},
+                    {latitude: 35.56317, longitude: 139.7053, icon: '' + url + '/mapicon/icon.png'}
+                ]
 		    });
 		});
 
