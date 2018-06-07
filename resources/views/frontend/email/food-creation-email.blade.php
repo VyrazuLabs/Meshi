1b8f7173d86f0c1d73b8e2265c086f30a3fe2578
ima0123 <atsushi@ima0123.com>
2018-06-07 23:47:12 +0900

    fix

--- resources/views/frontend/email/food-creation-email.blade.php
+++ resources/views/frontend/email/food-creation-email.blade.php
@@ -1,6 +1,7 @@
-<p style="margin-bottom: 0;margin-top: 0;">Hi, <b>Admin</b></p>
-<p style="margin-bottom: 0;margin-top: 0;">New food <b>{{$foodDetails->item_name}}</b> has been created by {{$foodDetails->creatorName}}</p>
-<p style="margin-bottom: 0;margin-top: 0;">Deliverable Area: {{$foodDetails->deliverable_area}}</p>
-<p style="margin-bottom: 0;margin-top: 0;">Delivery Date: {{$foodDetails->date_of_availability}}</p>
+<p style="margin-bottom: 0;margin-top: 0;">クリエーターさんのお料理が新しく登録されました。</p>
+<p style="margin-bottom: 0;margin-top: 0;">メシクリエーターさん: {{$foodDetails->creatorName}}</p>
+<p style="margin-bottom: 0;margin-top: 0;">お料理名: <b>{{$foodDetails->item_name}}</b></p>
+<p style="margin-bottom: 0;margin-top: 0;">提供エリア: {{$foodDetails->deliverable_area}}</p>
+<p style="margin-bottom: 0;margin-top: 0;">提供日: {{$foodDetails->date_of_availability}}</p>
 <p style="margin-bottom: 0;margin-top: 0;">Price: ¥{{$foodDetails->price}}</p>
-<p style="margin-bottom: 0;margin-top: 0;">Detail: ¥{{$foodDetails->food_description}}</p>
+<p style="margin-bottom: 0;margin-top: 0;">Detail: {{$foodDetails->food_description}}</p>
